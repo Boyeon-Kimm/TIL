67b1110cb6758981ee73e8bed8fd0016a80724ba
Boyeon-Kimm <kkbbyy1113@gmail.com>
2023-04-11 15:11:38 +0900

    0411

---
+++ 08spring/day0411/workspace/Spring_01_DI_1_개념/src/com/ssafy/di4/ComputerFactory.java
@@ -0,0 +1,14 @@
+package com.ssafy.di4;
+
+public class ComputerFactory {
+	//이러한 컴퓨터 줘봐잉
+	public static Computer getComputer(String type) {
+		if(type.equals("D")) {
+			return new Desktop();
+		}else if (type.equals("L")) {
+			return new Laptop();
+		}
+		return null;
+		
+	}
+}
---
+++ 08spring/day0411/workspace/Spring_01_DI_2_시작하기/src/com/ssafy/di/Test.java
@@ -0,0 +1,28 @@
+package com.ssafy.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		System.out.println("1~~");
+		Programmer p = (Programmer) context.getBean("programmer");
+		Desktop desktop = context.getBean("desktop", Desktop.class);
+		p.setComputer(desktop);
+		System.out.println("2~~");
+		
+		p.coding();
+//		
+//		
+//		Desktop d = context.getBean("desktop", Desktop.class);
+//		Desktop d2 = context.getBean("desktop", Desktop.class);
+//		
+//		System.out.println(d);
+//		System.out.println(d2);
+//		System.out.println(d == d2);
+		
+	}
+}
